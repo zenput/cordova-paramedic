28d5dac1d7adf2ba11027936421a71ede356fdce
Vladimir Kotikov <v-vlkoti@microsoft.com>
2016-06-02 12:02:06 +0300

    Add logic to enable package debugging mode on Windows

---
+++ debug-mode-plugin/scripts/enable-debug-mode.js
@@ -0,0 +1,46 @@
+/**
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+*/
+
+module.exports = function (context) {
+    var path = require('path');
+    var shell = context.requireCordovaModule('shelljs');
+
+    var libPath        = path.resolve(context.opts.projectRoot, "platforms/windows/cordova/lib");
+    var appUtilsPath   = path.join(libPath, "WindowsStoreAppUtils.ps1");
+    var appUtilsBackupPath   = path.join(libPath, "WindowsStoreAppUtils.ps1.bak");
+    var srcScriptPath  = path.join(__dirname, "EnableDebuggingForPackage.ps1");
+    var destScriptPath = path.join(libPath, "EnableDebuggingForPackage.ps1");
+
+    // copy over the patch
+    shell.cp("-f", srcScriptPath, libPath);
+    shell.cp("-f", appUtilsPath, appUtilsBackupPath);
+
+    // add extra code to patch
+    shell.sed(
+        "-i",
+        /^\s*\$appActivator .*$/gim,
+        "$&\n\n" +
+        "    # START ENABLE DEBUG MODE SECTION\n" +
+        "    powershell " + destScriptPath + " $$ID\n" +
+        "    $Ole32 = Add-Type -MemberDefinition '[DllImport(\"Ole32.dll\")]public static extern int CoAllowSetForegroundWindow(IntPtr pUnk, IntPtr lpvReserved);' -Name 'Ole32' -Namespace 'Win32' -PassThru\n" +
+        "    $Ole32::CoAllowSetForegroundWindow([System.Runtime.InteropServices.Marshal]::GetIUnknownForObject($appActivator), [System.IntPtr]::Zero)\n" +
+        "    # END ENABLE DEBUG MODE SECTION\n",
+        appUtilsPath
+    );
+};
